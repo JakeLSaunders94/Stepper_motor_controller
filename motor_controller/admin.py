e056973783e35e383bacda946e8cf52f9db1632d
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-09-29 16:39:20 +0100

    Format codebase.

--- motor_controller/admin.py
+++ motor_controller/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+"""Admin."""
+
+# Django
+from django.contrib import admin  # noqa: F401
 
 # Register your models here.
--- motor_controller/views.py
+++ motor_controller/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+"""Views."""
+
+# Django
+from django.shortcuts import render  # noqa: F401
 
 # Create your views here.
