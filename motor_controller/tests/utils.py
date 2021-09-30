e1488c4fd15fb842e84e1106eb3d1eb75d910916
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-09-30 14:16:10 +0100

    Add factory for StepperMotor

---
+++ motor_controller/tests/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Utilities to help with testing."""
+# 3rd-party
+import factory
+from factory.django import DjangoModelFactory
+
+# Project
+from motor_controller.constants import AVAILABLE_RPI_GPIO_PINS
+from motor_controller.constants import STEPPER_DRIVER_TYPES
+
+
+class StepperMotorFactory(DjangoModelFactory):
+    """Factory for StepperMotor model."""
+
+    name = factory.Sequence(lambda n: f"Stepper Motor {n}")
+    description = factory.Sequence(lambda n: f"Factory generated stepper Motor {n}")
+    driver_type = STEPPER_DRIVER_TYPES[0][0]
+    direction_GPIO_pin = AVAILABLE_RPI_GPIO_PINS[0][0]
+    step_GPIO_pin = AVAILABLE_RPI_GPIO_PINS[0][0]
