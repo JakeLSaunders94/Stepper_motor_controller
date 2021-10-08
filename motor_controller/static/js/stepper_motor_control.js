986f2f677643ee08d342988b0b49fa8c48d905b6
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-10-08 18:57:33 +0100

    That will probably work. Much cleanup required.

--- motor_controller/static/js/stepper_motor_control.js
+++ motor_controller/static/js/stepper_motor_control.js
@@ -16,6 +16,8 @@ async function postData(url = '', data = {}) {
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
+
+    // TODO - Handle errors here.
     return response.json(); // parses JSON response into native JavaScript objects
 }
 
