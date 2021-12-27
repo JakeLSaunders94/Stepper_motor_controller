7549cf84adfeae59463cea1f33870abcfc27eb06
jakesaunders <jakelsaunders94@gmail.com>
2021-12-27 21:41:07 +0000

    Added urls tests

--- motor_controller/tests/test_urls.py
+++ motor_controller/tests/test_urls.py
@@ -7,10 +7,21 @@ from unittest import TestCase
 # Django
 from django.urls import reverse
 
+from motor_controller.views import move_stepper_motor_ajax_view, stepper_motor_modal_ajax_view, \
+    stepper_motor_basic_control_view
+
 
 class TestURLs(TestCase):
     def test_urls(self):
         assert (
-            reverse("move_stepper_motor_ajax_view", args=[1])
+            reverse(move_stepper_motor_ajax_view, args=[1])
             == "motor-controller/stepper-motor-move/1"
+        )
+        assert (
+                reverse(stepper_motor_modal_ajax_view, args=[1])
+                == "motor-controller/stepper-motor-modal/1"
+        )
+        assert (
+                reverse(stepper_motor_basic_control_view, args=[1])
+                == "motor-controller/stepper-motor-control"
         )
