9b9ba3f3d7d165491fc3ff7a7df6f028c644782f
jakesaunders <jakelsaunders94@gmail.com>
2021-12-27 21:44:14 +0000

    Docstrings

--- motor_controller/tests/test_urls.py
+++ motor_controller/tests/test_urls.py
@@ -7,21 +7,25 @@ from unittest import TestCase
 # Django
 from django.urls import reverse
 
-from motor_controller.views import move_stepper_motor_ajax_view, stepper_motor_modal_ajax_view, \
-    stepper_motor_basic_control_view
+# Project
+from motor_controller.views import move_stepper_motor_ajax_view
+from motor_controller.views import stepper_motor_basic_control_view
+from motor_controller.views import stepper_motor_modal_ajax_view
 
 
 class TestURLs(TestCase):
+    """Tests for urls."""
+
     def test_urls(self):
+        """Test for urls."""
         assert (
             reverse(move_stepper_motor_ajax_view, args=[1])
             == "/motor-controller/stepper-motor-move/1"
         )
         assert (
-                reverse(stepper_motor_modal_ajax_view, args=[1])
-                == "/motor-controller/stepper-motor-modal/1"
+            reverse(stepper_motor_modal_ajax_view, args=[1])
+            == "/motor-controller/stepper-motor-modal/1"
         )
         assert (
-                reverse(stepper_motor_basic_control_view)
-                == "/motor-controller/stepper-motor-control"
+            reverse(stepper_motor_basic_control_view) == "/motor-controller/stepper-motor-control"
         )
